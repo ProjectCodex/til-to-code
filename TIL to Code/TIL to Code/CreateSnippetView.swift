94fe7ea4a6f7bfb637e644a0085c5f9de8a1ce7b
Chris Albanese <Christopher@CafeRE.com>
2018-10-04 21:38:48 -0400

    added files for core data model and other views

---
+++ TIL to Code/TIL to Code/CreateSnippetView.swift
@@ -0,0 +1,30 @@
+//
+//  CreateSnippetView.swift
+//  TIL to Code
+//
+//  Created by CHRISTOPHER ALBANESE on 10/4/18.
+//  Copyright © 2018 Project Codex. All rights reserved.
+//
+
+import UIKit
+
+class CreateSnippetView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TIL to Code/TIL to Code/PersistenceService.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceService.swift
+//  TIL to Code
+//
+//  Created by CHRISTOPHER ALBANESE on 10/4/18.
+//  Copyright © 2018 Project Codex. All rights reserved.
+//
+
+import Foundation
