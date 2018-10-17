0412e116290fb124a4c07fb4de49601fcc1699d7
ineczka <user145331@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-17 02:53:16 -0400

    update

--- Til to Code/Pods/Target Support Files/Pods-TIL to CodeTests/Pods-TIL to CodeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TIL_to_CodeTests : NSObject
-@end
-@implementation PodsDummy_Pods_TIL_to_CodeTests
-@end
--- Til to Code/Pods/Target Support Files/Pods-TIL to CodeUITests/Pods-TIL to CodeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TIL_to_CodeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TIL_to_CodeUITests
-@end
