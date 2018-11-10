87cda29ae47c93c2210f7e80e115c7faca9aafa6
ineczka <user145331@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-11-09 19:03:39 -0500

    update master

--- TIL to Code/TIL to Code/AppDelegate.swift
+++ TIL to Code/TIL to Code/AppDelegate.swift
@@ -25,6 +25,8 @@
 //
 
 import UIKit
+import Firebase
+		
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -34,6 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
---
+++ Til to Code/Pods/Target Support Files/Pods-TIL to CodeTests/Pods-TIL to CodeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TIL_to_CodeTests : NSObject
+@end
+@implementation PodsDummy_Pods_TIL_to_CodeTests
+@end
